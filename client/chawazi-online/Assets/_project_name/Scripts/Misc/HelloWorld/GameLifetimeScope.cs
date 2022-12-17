ba30270d568440c5d440fc7a2267189065f7462d
ElisarEisenbach <elisar.eisenbach@gmail.com>
2022-12-17 23:35:32 +0200

    add firebase analytics (#1)
    change unity version to 2021.3.15
    firebase plugins are ignored

--- client/chawazi-online/Assets/_project_name/Scripts/Misc/HelloWorld/GameLifetimeScope.cs
+++ client/chawazi-online/Assets/_project_name/Scripts/Misc/HelloWorld/GameLifetimeScope.cs
@@ -10,6 +10,26 @@ public class GameLifetimeScope : LifetimeScope
 
     protected override void Configure(IContainerBuilder builder)
     {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available) {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                var app = Firebase.FirebaseApp.DefaultInstance;
+                Debug.Log("Elisar was here");
+                // Log an event with a float parameter
+                Firebase.Analytics.FirebaseAnalytics
+                    .LogEvent(Firebase.Analytics.FirebaseAnalytics.EventLevelEnd);
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            } else {
+                UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+        
+        
         builder.RegisterComponent(inputManager);
         builder.Register<DefaultUnityLogger>(Lifetime.Singleton).AsImplementedInterfaces();
       //  builder.RegisterComponentInHierarchy<DummyTouch>();
