45160d7619e8ea3463fc3f5bf82adcd386dc89cb
ElisarEisenbach <elisar.eisenbach@gmail.com>
2023-04-14 01:14:20 +0300

    add meta

--- client/chawazi-online/Assets/_project_name/Services/Analytics/AnalyticsScriptableObject.cs
+++ client/chawazi-online/Assets/_project_name/Services/Analytics/AnalyticsScriptableObject.cs
@@ -5,6 +5,6 @@ namespace _project_name.Services.Analytics
     [CreateAssetMenu(fileName = "analytics_enabled", menuName = "Analytics", order = 0)]
     public class AnalyticsScriptableObject : ScriptableObject
     {
-        public bool isEnabled;
+        public bool isEnabled = true;
     }
 }
