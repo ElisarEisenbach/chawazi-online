af4c20dc9a3f17c693f9643d4fdc222aa9b01cd5
ElisarEisenbach <elisar.eisenbach@gmail.com>
2023-04-15 23:10:30 +0300

    formatting

--- client/chawazi-online/Assets/_project_name/Services/Analytics/SettingsScriptableObject.cs
+++ client/chawazi-online/Assets/_project_name/Services/Analytics/SettingsScriptableObject.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "settings_file", menuName = "Settings Objects/Empty", order = 0)]
 public class SettingsScriptableObject : ScriptableObject
 {
-    [FormerlySerializedAs("isEnabled")] public bool isAnalyticsEnabled = true;
+    public bool isAnalyticsEnabled = true;
 }
